7857cf26e7305f8697e454af4099753eeff0ba38
GyuChangSeong <ahdzlq12@gmail.com>
2023-07-06 18:33:51 +0900

    서블릿테스트 html

---
+++ src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,12 @@
+package hello.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloData {
+//json데이터로 활용 할 예정
+    private String username;
+    private int age;
+}
